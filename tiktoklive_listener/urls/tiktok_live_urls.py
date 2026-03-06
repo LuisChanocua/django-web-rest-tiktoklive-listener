8c82bc405881a6fd3da35900369fa2683d8c7e17
LuisChanocua <esteban083057@gmail.com>
2026-03-05 23:31:52 -0600

    feat: proxy test looker studio proxy

--- tiktoklive_listener/urls/tiktok_live_urls.py
+++ tiktoklive_listener/urls/tiktok_live_urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from ..views.tiktok_comment_views import TikTokCommentInView
-from ..views.tiktok_live_view import ActiveListenerStreamersView, ListenerToggleView
+from ..views.tiktok_live_view import ActiveListenerStreamersView, ListenerToggleView, daily_products_proxy
 
 urlpatterns = [
     path("comments/", TikTokCommentInView.as_view(), name="tiktok-comments-in"),
     path("active-streamers/", ActiveListenerStreamersView.as_view(), name="tiktok-active-streamers"),
-    path("lister/toggle/", ListenerToggleView.as_view(), name="tiktok-listener-toggle"),
+    path("listener/toggle/", ListenerToggleView.as_view(), name="tiktok-listener-toggle"),
+    path("proxy/daily-products/", daily_products_proxy, name="proxy-daily-products"),
 ]
