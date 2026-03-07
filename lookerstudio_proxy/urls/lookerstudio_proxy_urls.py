3ec7b1be26a6c639ff3bbd3ecf49eb25e2f15884
LuisChanocua <esteban083057@gmail.com>
2026-03-07 00:42:02 -0600

    feat: daily users proxy endpoint

--- lookerstudio_proxy/urls/lookerstudio_proxy_urls.py
+++ lookerstudio_proxy/urls/lookerstudio_proxy_urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from lookerstudio_proxy.views.views import daily_products_proxy
+from lookerstudio_proxy.views.views import daily_products_proxy, daily_users_proxy
 
 urlpatterns = [
     path('products/daily-products/', daily_products_proxy, name='proxy-products-daily-products'), 
+    path('users/daily-users/', daily_users_proxy, name='proxy-users-daily-users'), 
 ]
--- lookerstudio_proxy/views/views.py
+++ lookerstudio_proxy/views/views.py
@@ -2,12 +2,16 @@ import os
 
 from django.shortcuts import render
 from django.http import JsonResponse
+from rest_framework import status
 import requests
 
 def daily_products_proxy(request):
-    dotnet_url = os.getenv('API_INTERNAL_TOKEN', 'https://stage.promokelloggs.com/LookerStudio/DailyProducts')
+    endpoint_suffix = 'LookerStudio/DailyProducts'  # Cambia esto según el endpoint que quieras usar
+    apiUrl = os.getenv(f'API_BASE_URL_KELLANOVA', f'https://stage.promokelloggs.com/')
+       
+    apiCallUrl = f'{apiUrl}{endpoint_suffix}'
     print('method:', request.method)
-    print('url:', dotnet_url)
+    print('url:', apiCallUrl)
     print('headers:', request.headers)
 
     method = request.method
@@ -19,17 +23,51 @@ def daily_products_proxy(request):
 
     try:
         if method == 'GET':
-            response = requests.get(dotnet_url, headers=forced_headers, params=request.GET)
+            response = requests.get(apiCallUrl, headers=forced_headers, params=request.GET)
 
         elif method == 'POST':
             body = request.body
-            response = requests.post(dotnet_url, headers=forced_headers, data=body)
+            response = requests.post(apiCallUrl, headers=forced_headers, data=body)
 
         else:
-            return JsonResponse({'success': False, 'message': 'Método no permitido'}, status=405)
+            return JsonResponse({'success': False, 'message': 'Método no permitido'}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
 
         print(response.json())
         return JsonResponse(response.json(), status=response.status_code, safe=False)
 
     except Exception as e:
-        return JsonResponse({'success': False, 'error': str(e)}, status=500)
+        return JsonResponse({'success': False, 'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+    
+def daily_users_proxy(request):
+    
+    endpoint_suffix = 'LookerStudio/DailyUsers'  # Cambia esto según el endpoint que quieras usar
+    apiUrl = os.getenv(f'API_BASE_URL_KELLANOVA', f'https://stage.promokelloggs.com/')
+    
+    apiCallUrl = f'{apiUrl}{endpoint_suffix}'
+    print('method:', request.method)
+    print('url:', apiCallUrl)
+    print('headers:', request.headers)
+
+    method = request.method
+    incoming_headers = dict(request.headers)
+    forced_headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)',
+        'Content-Type': incoming_headers.get('Content-Type', 'application/json')
+    }
+
+    try:
+        if method == 'GET':
+            response = requests.get(apiCallUrl, headers=forced_headers, params=request.GET)
+
+        elif method == 'POST':
+            body = request.body
+            response = requests.post(apiCallUrl, headers=forced_headers, data=body)
+
+        else:
+            return JsonResponse({'success': False, 'message': 'Método no permitido'}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
+
+        print(response.json())
+        return JsonResponse(response.json(), status=response.status_code, safe=False)
+
+    except Exception as e:
+        return JsonResponse({'success': False, 'error': str(e)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
